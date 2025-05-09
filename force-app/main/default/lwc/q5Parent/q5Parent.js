0576b23224ebe11c49f76ea89a8bdef06ebb4e79
Deepgit731 <khandelwaldeepanshu19@gmail.com>
2025-05-09 10:18:09 +0530

    Delete force-app/main/default/lwc/q5Parent directory

--- force-app/main/default/lwc/q5Parent/q5Parent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Q5Parent extends LightningElement {
-        childData;
-    
-        handleDataFromChild(event) {
-            this.childData = event.detail;
-        }
-}
