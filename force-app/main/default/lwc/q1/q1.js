19fda2618dc9f930eb1e83c8482019ea5ee6ef8e
Deepgit731 <khandelwaldeepanshu19@gmail.com>
2025-05-09 10:17:36 +0530

    Delete force-app/main/default/lwc/q1 directory

--- force-app/main/default/lwc/q1/q1.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Q1 extends LightningElement {
-    label = "Click Me";
-    handleClick(){
-        this.label ="Clicked";
-
-    }
-}
